86b786aa34e9784f0d8328083ba6d4e0c038074a
Jair Herrera <grajale109@gmail.com>
2023-11-01 13:54:11 -0600

    new http

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -8,7 +8,8 @@ export const corsMiddlewares = () =>
         "http://localhost:1234",
         "http://localhost:3000",
         "http://127.0.0.1:5500",
-        "http://127.0.0.1:5501"
+        "http://127.0.0.1:5501",
+        "http://localhost:9224"
       ];
 
       if (ACCEPTED_ORIGINS.includes(origin)) {
